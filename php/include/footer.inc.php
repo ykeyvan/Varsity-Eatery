fe6aa4608bcce7d0124cf507405823149f8a0001
rkeyvan <rkeyvan@outlook.com>
2024-09-17 00:33:21 -0700

    Delete php directory

--- php/include/footer.inc.php
+++
@@ -1,7 +0,0 @@
-	<footer class="alcove">
-		<span class="alcove__item"><a class="alcove__link" href="terms-conditions.php">Terms & Conditions</a>&ensp;|</span>
-		<span class="alcove__item"><a class="alcove__link" href="private-policy.php">Privacy Policy</a>&ensp;|</span>
-    <span class="alcove__item">Copyright &copy; 2024, Varsity Eatery. All Rights Reserved&ensp;|</span>
-	</footer>
-</body>
-</html>
